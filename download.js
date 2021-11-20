6feaed076aa1ce993313512608210fe1ce47fa23
Filip Troníček <filip@gitpod.io>
2021-11-20 11:12:19 +0000

    Add progress bars

--- download.js
+++ download.js
@@ -1,12 +1,9 @@
 // @ts-check
 
-const fs = require('fs'),
-    http = require('http'),
-    https = require('https');
-const Stream = require('stream').Transform;
 const fetch = require('node-fetch');
 const getUrlsToArray = require("get-urls-to-array");
 const cp = require('child_process');
+const cliProgress = require('cli-progress');
 
 /**
  * @param {string} command
@@ -33,12 +30,12 @@ const exec = async (command, options) => {
 
 let at = 0;
 const step = 100;
+const pBar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
 
 const mimeType = encodeURIComponent("image/jpeg");
 const fetchNew = () => {
-    console.log("Fetching")
+    console.log(`Fetching page ${at / step + 1}`)
     fetch(`https://commons.wikimedia.org/w/index.php?title=Special:MIMESearch&limit=${step}&offset=${at}&mime=${mimeType}`).then((res) => {
-        console.log(res.status)
         if (res.ok) {
             return res.text();
         } else {
@@ -46,12 +43,14 @@ const fetchNew = () => {
         }
     }).then(async (data) => {
         const images = getUrlsToArray(data).filter(url => new URL(url).hostname === 'upload.wikimedia.org');
+        pBar.start(step, 0);
         for (const image of images) {
-            console.log(image);
+            pBar.increment(1);
             try {
-                await exec(`wget -P output/ '${image.slice(0, 300)}'`);
+                await exec(`wget -P output/ --quiet '${image.slice(0, 300)}'`, {quiet: true});
             } catch { }
         }
+        pBar.stop();
         at = at + step;
         fetchNew();
     });
